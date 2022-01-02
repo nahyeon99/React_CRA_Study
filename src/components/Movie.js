e19d5080d856a408b08692d8c27b1a8fba8ba347
NaHyeon <nahyeonee99@gmail.com>
2022-01-03 01:39:38 +0900

    React Router

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,12 +1,19 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function Movie({ coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
-      <h2>{title}</h2>
+      <h2>
+        <Link to="/movie">{title}</Link>
+      </h2>
       <p>{summary}</p>
-      <ul>{genres && genres.map((g) => <li key={g}>{g}</li>)}</ul>
+      <ul>
+        {genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
     </div>
   );
 }
